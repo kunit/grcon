3218f295caddac74f9c9f6a313909927748b0af2
TAKAHASHI Kunihiko <kunihiko.takahashi@gmail.com>
2018-12-21 18:22:03 +0900

    動くようになった

--- go.mod
+++ go.mod
@@ -1,1 +1,12 @@
 module github.com/kunit/grcon
+
+require (
+	github.com/containerd/cgroups v0.0.0-20181219155423-39b18af02c41
+	github.com/coreos/go-systemd v0.0.0-20181031085051-9002847aa142 // indirect
+	github.com/docker/go-units v0.3.3 // indirect
+	github.com/godbus/dbus v5.0.1+incompatible // indirect
+	github.com/gogo/protobuf v1.2.0 // indirect
+	github.com/opencontainers/runtime-spec v0.1.2-0.20181111125026-1722abf79c2f
+	github.com/pkg/errors v0.8.0 // indirect
+	golang.org/x/sys v0.0.0-20181220204120-b00e65af1da0 // indirect
+)
--- main.go
+++ main.go
@@ -5,24 +5,20 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"pkg/mod/github.com/opencontainers/runtime-spec@v1.0.1/specs-go"
 
 	"github.com/containerd/cgroups"
+	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
 
 func main() {
 	var (
-		cpu = flag.Int("cpu", 30, "cpu")
-		memory = flag.Int64("memory", 512 * 1024 * 1024, "memory")
-		user = flag.String("user", "", "user")
+		cpu     = flag.Int("cpu", 30, "cpu")
+		memory  = flag.Int64("memory", 512*1024*1024, "memory")
+		user    = flag.String("user", "", "user")
 		command = flag.String("command", "", "command")
 	)
 	flag.Parse()
 
-	fmt.Println(*cpu)
-	fmt.Println(*memory)
-	fmt.Println(*command)
-	fmt.Println(*user)
 	if len(*command) == 0 {
 		fmt.Println("command not found: --command [command]")
 		os.Exit(1)
@@ -32,22 +28,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	pid := os.Getpid
-	fmt.Printf("プロセスID: %d\n", pid)
-
-	shares := uint64(*cpu)
+	quota := int64(*cpu) * 1000
+	limit := int64(*memory)
 	control, err := cgroups.New(cgroups.V1, cgroups.StaticPath("/grcon"), &specs.LinuxResources{
-		CPU: &specs.CPU{
-			Shares: &shares,
+		CPU: &specs.LinuxCPU{
+			Quota: &quota,
+		},
+		Memory: &specs.LinuxMemory{
+			Limit: &limit,
 		},
 	})
-	defer control.Delete()
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	if err := control.Add(cgroups.Process{Pid:pid}); err != nil {
+	defer control.Delete()
+
+	if err := control.Add(cgroups.Process{Pid: os.Getpid()}); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
